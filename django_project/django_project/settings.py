8fdea121fb28db475018fa08c8309d1888cf91c3
Ubuntu <ubuntu@ip-172-31-16-162.us-west-2.compute.internal>
2022-09-17 18:46:23 +0000

    add docker file

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8*)yiv7_2qiw%x3%4y5k3@_#)nwatoyf+$t3=pu!-*%uf8mw8m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
